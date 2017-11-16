564cddb6d3f5f8a6c4869b8836b41aed306188d2
Constance Eger <egerconstance@gmail.com>
2017-11-16 15:42:05 -0500

    order form added. Bootstrap formatting breaking

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 
 
-/////*****function validateForm() {var x = document.forms["myForm"]["fname"].value;if (x == "") {alert("Name must be filled out");return false; }}****/////
+/*--- form sumbit ---*/ 
+function validateForm() {
+		var x = document.forms["myForm"]["fname"].value;
+		if (x == "") {alert("Name must be filled out");
+		return false;
+	}
+}
+
 
 
 var $overlay = $('<div id="overlay"></div>');
